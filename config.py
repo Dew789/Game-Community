240442cdac844444f219c824ab8c9ff19385e2cf
Rick <z86hang@163.com>
2017-11-21 12:30:58 +0800

    重新上传config

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = 'jiasd#$iasodjm4592zoxi3df'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 994
+    MAIL_USE_SSL = True
+    MAIL_USERNAME =  os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD =  os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[游戏社区]'
+    FLASKY_MAIL_SENDER = 'atestsomething@163.com'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    POSTS_PER_PAGE = 10
+    
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+        
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}      
